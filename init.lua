2a7868aea7727bf236a2284b3bf813c41fdcbce0
Zemtzov7 <72821250+zmv7@users.noreply.github.com>
2023-08-23 20:09:10 +0500

    Renovation

--- init.lua
+++ init.lua
@@ -1,33 +1,33 @@
 noclipsus = {}
+local nodes = {["default:stone"] = true, ["default:desert_stone"] = true, ["default:sand"] = true, ["default:dirt"] = true}
 local stop = false
-local function report(name,ppos,node,speed)
-    local text = "[antinoclip] "..name.." was sus noclipping in "..node.name.." at "..core.pos_to_string(vector.round(ppos))..", speed: "..core.pos_to_string(vector.round(speed,""))
-core.log("action",text)
-sendWebhook({content = '`'..text..'`'})
+local function report(name,ppos,node,vel)
+    local text = "[noclipdetect] "..name.." was sus noclipping in "..node.name.." at "..core.pos_to_string(vector.round(ppos))..", velocity: "..core.pos_to_string(vector.round(vel,""))
+    core.log("action",text)
 end
-core.register_globalstep(function()
- for _,player in ipairs(core.get_connected_players()) do
-    local name = player:get_player_name()
-    if not name then return end
-    if core.check_player_privs(name,{noclip = true}) then return end
-    local ppos = player:get_pos()
-    local node = core.get_node_or_nil(ppos)
-    if not node then return end
-    if node.name == "default:stone" or node.name == "default:desert_stone" or node.name == "default:sand" or node.name == "default:dirt" or node.name == "default:sandstone" then
-    local speed = player:get_velocity()
-    if speed.x >= 10 or speed.z >= 10 then
-    if noclipsus[name] == true then
-    report(name,ppos,node,speed)
-    noclipsus[name] = false
-    elseif stop == false then
-    core.after(2,function()
-    noclipsus[name] = true
-    stop = false
-    end)
-    stop = true
+core.register_globalstep(function(dtime)
+    for _,player in ipairs(core.get_connected_players()) do
+        local name = player:get_player_name()
+        if name then
+            if not core.check_player_privs(name,{noclip = true}) then
+                local ppos = player:get_pos()
+                local node = core.get_node_or_nil(ppos)
+                if node then
+                    if nodes[node.name] then
+                        local vel = player:get_velocity()
+                        if vel.x >= 10 or vel.z >= 10 then
+                            if noclipsus[name] then
+                                report(name,ppos,node,speed)
+                                noclipsus[name] = nil
+                            else
+                                core.after(2, function()
+                                    noclipsus[name] = true
+                                end)
+                            end
+                        end
+                    end
+                end
+            end
+        end
     end
-    end
-    
- end
- end
 end)
